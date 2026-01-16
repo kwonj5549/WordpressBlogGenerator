971db8f2f8d37382c92be7c9d17906a451398777
kwonj5549 <kwonj5549@gmail.com>
2026-01-15 20:50:08 -0600

    Initial Commit

---
+++ WordpressBlogGenerator/WordpressBlogGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordpressBlogGeneratorApp.swift
+//  WordpressBlogGenerator
+//
+//  Created by Junwoo Kwon on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct WordpressBlogGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
